d885051759f30fcf8f0664e78cfdbb17e61b8f83
Pive0528 <pive0528@gmail.com>
2024-10-30 14:30:00 +0900

    오류 수정

--- src/main/java/com/example/rest_24_10/base/entity/BaseEntity.java
+++ src/main/java/com/example/rest_24_10/base/entity/BaseEntity.java
@@ -1,5 +1,8 @@
+package com.example.rest_24_10.base.entity;
+
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,9 +11,12 @@ import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.time.LocalDateTime;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
+
 @Getter
 @SuperBuilder
 @MappedSuperclass
