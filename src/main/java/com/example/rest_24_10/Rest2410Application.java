7717fcd3607bae3ecddf7511a6a869092566972f
Pive0528 <pive0528@gmail.com>
2024-10-30 13:51:41 +0900

    스프링 시큐리티 설정 끄기, member login test

--- src/main/java/com/example/rest_24_10/Rest2410Application.java
+++ src/main/java/com/example/rest_24_10/Rest2410Application.java
@@ -1,13 +1,11 @@
-package com.koreait.rest_24_10;
+package com.example.rest_24_10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Rest2410Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Rest2410Application.class, args);
     }
-
 }
