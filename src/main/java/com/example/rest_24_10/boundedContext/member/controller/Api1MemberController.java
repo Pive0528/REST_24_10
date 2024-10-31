f8e8eeb539663304e57956fa1d934691e354a989
Pive0528 <pive0528@gmail.com>
2024-10-31 13:51:52 +0900

    API 버저닝

---
+++ src/main/java/com/example/rest_24_10/boundedContext/member/controller/Api1MemberController.java
@@ -0,0 +1,49 @@
+package com.example.rest_24_10.boundedContext.member.controller;
+
+import com.example.rest_24_10.base.rsData.RsData;
+import com.example.rest_24_10.boundedContext.member.service.MemberService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping(value = "/api/v1/member", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+public class Api1MemberController {
+
+    private final MemberService memberService;
+
+    @Data
+    public static class LoginRequest {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class LoginResponse {
+        private final String accessToken;
+    }
+
+    @PostMapping("/login")
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse resp) {
+        String accessToken = memberService.genAccessToken(loginRequest.getUsername(), loginRequest.getPassword());
+
+//        resp.addHeader("Authentication", accessToken);
+
+        return RsData.of("S-1", "액세스 토큰 생성됨", new LoginResponse(accessToken));
+    }
+
+}
